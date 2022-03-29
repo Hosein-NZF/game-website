05434e4bb1fc6fc5a005068c05a36dbe09b0b473
HoseinNz <HoseinnNazifi@gmail.com>
2022-03-29 05:22:55 +0430

    rename folder of static files

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,11 @@ export default {
   //   port:'8000'
   // },
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  mode: "static",
+  router: {
+    base: "sample-project",
+  },
+  publicPath: 'static/',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
